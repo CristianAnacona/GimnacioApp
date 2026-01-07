0d46218804429e93a345eb0ceb1af66f0b865e4b
Cristian Anacona <cristianfelipe07@outlook.com>
2026-01-07 15:15:02 -0500

    feat: se agrega modificaciones para las rutas protegidas

--- src/app/guards/auth.ts
+++ src/app/guards/auth.ts
@@ -1,17 +1,21 @@
-
 import { inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, CanActivateFn } from '@angular/router';
 
-export const authGuard = () => {
+export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
-  const token = localStorage.getItem('token'); // Busca el permiso en el navegador
+  const token = localStorage.getItem('token');
+  const role = localStorage.getItem('role')?.toLowerCase().trim();
 
-  if (token) {
-    return true; // Si hay token, ¡pasa adelante!
-  } else {
-    // Si no hay token, lo mandamos al login y bloqueamos el paso
+  if (!token) {
     router.navigate(['/login']);
     return false;
   }
-};
 
+  // Si intenta ir a cualquier ruta que empiece con /admin y NO es admin
+  if (state.url.includes('/admin') && role !== 'admin') {
+    router.navigate(['/socio']); // Lo mandamos a su ruta permitida
+    return false;
+  }
+
+  return true;
+};
